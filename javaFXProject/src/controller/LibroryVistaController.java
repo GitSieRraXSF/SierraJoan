077f5750902c6f3d8b6b48c20a1b558dca20cfb5
ESTUDIANTE <ESTUDIANTE@SI2-17>
2025-04-08 07:43:31 -0500

    Commit

--- javaFXProject/src/controller/LibroryVistaController.java
+++ javaFXProject/src/controller/LibroryVistaController.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 
 import application.Main;
 import data.BookDAO;
-import data.BookDataManager;
 import data.DatabaseConnection;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
